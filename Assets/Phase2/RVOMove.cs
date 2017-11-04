2ffe6f02187cd184f747e2d423cab41cdac85664
mp3242 <mp3242@columbia.edu>
2017-11-03 20:53:45 -0400

    attempt fixing rvo

--- Assets/Phase2/RVOMove.cs
+++ Assets/Phase2/RVOMove.cs
@@ -6,7 +6,7 @@ using System;
 using Random = UnityEngine.Random;
 
 public class RVOMove : MonoBehaviour {
-	public int NUM_TEST_VELOCITIES = 100;
+	public int NUM_TEST_VELOCITIES = 20;
 	Vector3[] testVelocities;
 	public Transform goal;
 
@@ -18,12 +18,12 @@ public class RVOMove : MonoBehaviour {
 
 	Collider[] hitColliders;
 	List<GameObject> targetsInRange = new List<GameObject>(); //DS
-	const int GET_TARGET_RANGE = 5;
+	const int GET_TARGET_RANGE = 10;
 	double w = 1; //aggressiveness
 
 	void Start () {
 		pos_a = transform.position;
-		pos_a_prev = transform.position;
+		pos_a_prev = transform.position - vel_a_max * Time.deltaTime;
 		speed_a_max = vel_a_max.magnitude;
 	}
 
@@ -31,9 +31,15 @@ public class RVOMove : MonoBehaviour {
 		//assumes only other cylinders exist
 		hitColliders = Physics.OverlapSphere (gameObject.transform.position, GET_TARGET_RANGE);
 		if (hitColliders != null)
-			targetsInRange = hitColliders.Select (c => c.gameObject).ToList ();
+			targetsInRange = hitColliders.Where(c => c.gameObject != gameObject).Select (c => c.gameObject).ToList ();
 		else
 			targetsInRange = null;
+	}
+
+	Vector3 getDirections () {
+		Vector3 diff = goal.position - this.transform.position;
+		float dist = diff.magnitude;
+		return diff / dist;
 	}
 
 	double GetMinTimeToCollision(Vector3 vel_a_prime) {
@@ -53,33 +59,40 @@ public class RVOMove : MonoBehaviour {
 			double b = 2 * Vector3.Dot(vel, pos_a - pos_b);
 			double c = Vector3.Dot(pos_a, pos_a) + Vector3.Dot(pos_b, pos_b) - 2 * Vector3.Dot(pos_a, pos_b) - Math.Pow(r_ab, 2);
 
+//			Debug.Log ("Pos_a " + pos_a);
+//			Debug.Log ("Vel_a " + vel_a);
+//			Debug.Log ("Pos_b " + pos_b);
+//			Debug.Log ("Vel_b " + vel_b);
+//			Debug.Log ("Vel " + vel);
+
 			double disc = Math.Pow(b, 2) - 4 * a * c;
 			if (disc < 0) {
 				timesToCollision[i] = -1d;
 				continue;
 			}
 			double sqrt_disc = Math.Sqrt (disc);
+//			Debug.Log ("a " + a + "    b " + b + "    c " + c);
 			timesToCollision[i] = (-b - sqrt_disc) / (2 * a);
 		}
-		foreach(GameObject target in targetsInRange)
-		{
-			Debug.Log (target.name);
-		}
-		Debug.Log (timesToCollision);
-		timesToCollision = Array.FindAll(timesToCollision, time => time > 0);
-		Debug.Log (timesToCollision);
-		if (timesToCollision != null)
+		if (timesToCollision.Length > 0)
 			return timesToCollision.Min ();
 		else
 			return -1d;
 	}
 
+	//HERERE
+	//ONLY GenerateTESTVELOCITIES when on collision course (when tc of current vel_a is not infinity)
+	//Check out when the tc of generated test vels are nonzero
+
 	void GenerateTestVelocities () {
 		testVelocities = new Vector3[NUM_TEST_VELOCITIES];
 		for (int i = 0; i < testVelocities.Length; i++) {
-			float x = Random.Range (0f, (float)speed_a_max);
-			float z = Random.Range (0f, (float)Math.Sqrt (vel_a_max.sqrMagnitude - x * x));
-			testVelocities [i] = new Vector3 (x,0,z);
+			float x = Random.Range (-(float)speed_a_max, (float)speed_a_max);
+			float rand_z = (float)Math.Sqrt (vel_a_max.sqrMagnitude - Math.Pow(x,2));
+			float z = Random.Range (-rand_z, rand_z);
+			Vector3 v = new Vector3 (x,0,z);
+			Debug.DrawRay(transform.position, v, Color.green);
+			testVelocities [i] = v;
 		}
 	}
 
@@ -99,18 +112,20 @@ public class RVOMove : MonoBehaviour {
 			if (tc > 0)
 				penalty_tc = w / tc;
 
+			//Vector3.Scale(getDirection(), vel_a_max)
 			penalty_stray = (vel_a_max - testVelocities[i]).magnitude;
+			Debug.Log ("tc: " + penalty_tc + " stray: " + penalty_stray);
 			penalties [i] = penalty_tc + penalty_stray;
 		}
 
-		double? maxVal = null; //nullable so this works even if you have all super-low negatives
+		double? minVal = null; //nullable so this works even if you have all super-low negatives
 		int index = -1;
 		for (int i = 0; i < penalties.Length; i++)
 		{
 			double thisNum = penalties[i];
-			if (!maxVal.HasValue || thisNum > maxVal.Value)
+			if (!minVal.HasValue || thisNum < minVal.Value)
 			{
-				maxVal = thisNum;
+				minVal = thisNum;
 				index = i;
 			}
 		}
@@ -125,6 +140,8 @@ public class RVOMove : MonoBehaviour {
 
 		Vector3 vel_a_new = EstimateOptimalNewVelocity ();
 		Vector3 step = vel_a_new * Time.deltaTime;
+//		Vector3 step = vel_a_max * Time.deltaTime;
+		Debug.Log ("------- " + gameObject.name + " --------");
 
 		pos_a_prev = transform.position;
 		transform.position += step;
