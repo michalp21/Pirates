2e807a8dd316142ac5c6e2f03c7a8d17a674f924
mp3242 <mp3242@columbia.edu>
2018-01-22 15:05:36 -0500

    Switch from Phase1 to Phase2 on countdown

---
+++ Assets/Phase1/SceneMaster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMaster : MonoBehaviour {
+
+    void Phase1ToPhase2()
+    {
+        SceneManager.LoadScene("Phase2");
+        Debug.Log("wakawakawaka");
+    }
+
+	// Use this for initialization
+	void Start () {
+        Timer.onTimerZero += Phase1ToPhase2;
+	}
+
+    void OnDisable()
+    {
+        Timer.onTimerZero -= Phase1ToPhase2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Phase1/Timer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Credit: rober-psious
+public class Timer : MonoBehaviour
+{
+    public int Minutes = 0;
+    public int Seconds = 0;
+
+    private Text m_text;
+    private float m_leftTime;
+
+    public delegate void UnitEventHandler();
+    public static event UnitEventHandler onTimerZero;
+
+    private void Awake()
+    {
+        m_text = GetComponent<Text>();
+        m_leftTime = GetInitialTime();
+    }
+
+    private void Update()
+    {
+        if (m_leftTime > 0f)
+        {
+            //  Update countdown clock
+            m_leftTime -= Time.deltaTime;
+            Minutes = GetLeftMinutes();
+            Seconds = GetLeftSeconds();
+
+            //  Show current clock
+            if (m_leftTime > 0f)
+            {
+                m_text.text = "Time : " + Minutes + ":" + Seconds.ToString("00");
+            }
+            else
+            {
+                //  The countdown clock has finished
+                m_text.text = "Time : 0:00";
+            }
+        }
+        else
+        {
+            if (onTimerZero != null)
+                onTimerZero();
+        }
+    }
+
+    private float GetInitialTime()
+    {
+        return Minutes * 60f + Seconds;
+    }
+
+    private int GetLeftMinutes()
+    {
+        return Mathf.FloorToInt(m_leftTime / 60f);
+    }
+
+    private int GetLeftSeconds()
+    {
+        return Mathf.FloorToInt(m_leftTime % 60f);
+    }
+}
